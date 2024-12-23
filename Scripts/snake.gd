f50a17cbdbeff6d2bc89ae446eaf84116526052c
Zakarya Meddad <zakarya.meddad@gmail.com>
2024-12-23 13:30:24 +0100

    added snappy movement

--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -8,30 +8,39 @@ enum Direction {
 }
 
 @export var move_speed: int = 50
+@export var tile_size: int = 1
+@export var move_cooldown: float = 0.3
+@export var segment: PackedScene
 
-@onready var area: Area2D = $CharacterBody2D
+@onready var area: Area2D = $Body
 @onready var sprite: Sprite2D = $Sprite2D
 
 var current_direction: Direction
-var movement_vector: Vector2i
+var movement_vector: Vector2
+var time_since_move = 0
 
 func _ready():
 	current_direction = Direction.UP
 	movement_vector = Vector2i.UP
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
 
 func _input(event):
 	if event.is_action_pressed("ui_up") and current_direction != Direction.DOWN:
-		movement_vector = Vector2i.UP
+		movement_vector = Vector2.UP
 		current_direction = Direction.UP
 	if event.is_action_pressed("ui_left") and current_direction != Direction.RIGHT:
-		movement_vector = Vector2i.LEFT
+		movement_vector = Vector2.LEFT
 		current_direction = Direction.LEFT
 	if event.is_action_pressed("ui_down") and current_direction != Direction.UP:
-		movement_vector = Vector2i.DOWN
+		movement_vector = Vector2.DOWN
 		current_direction = Direction.DOWN
 	if event.is_action_pressed("ui_right") and current_direction != Direction.LEFT:
-		movement_vector = Vector2i.RIGHT
+		movement_vector = Vector2.RIGHT
 		current_direction = Direction.RIGHT
+	if event.is_action_pressed("ui_accept"):
+		pass
+		
 
 func _process(delta):
 	match current_direction:
@@ -45,4 +54,8 @@ func _process(delta):
 			sprite.frame = 3
 
 func _physics_process(delta):
-	translate(movement_vector * move_speed * delta)
+	if time_since_move > move_cooldown:
+		position += movement_vector * tile_size * move_speed
+		time_since_move = 0
+	else:
+		time_since_move += delta
